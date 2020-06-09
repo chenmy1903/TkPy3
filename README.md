69f8a658c2550d09e6008c607f20ee5e837db523
chenmy10 <chenmy10@outlook.com>
2020-06-09 16:22:48 +0800

    Signed-off-by: chenmy10 <chenmy10@outlook.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 安装依赖
 ```commandline
-pip install PyQt5
+pip install PyQt5 -i https://pypi.doubanio.com/simple
 python -m TkPy3.tkpy3_tools.relys
 ```
 ## 运行TkPy3
--- TkPy3/tkpy3_tools/pip_tools.py
+++ TkPy3/tkpy3_tools/pip_tools.py
@@ -7,16 +7,16 @@ class tkpy_pip(object):
         object.__init__(self)
 
     def install(self, *packages):
-        return PipMain(['install', *packages])
+        return PipMain(['install', *packages, '-i https://pypi.doubanio.com/simple'])
 
     def uninstall(self, *packages):
         return PipMain(['uninstall', *packages, '-y'])
 
     def force_reinstall(self, *packages):
-        return PipMain(['install', *packages, '--force-reinstall'])
+        return PipMain(['install', *packages, '--force-reinstall', '-i https://pypi.doubanio.com/simple'])
 
     def upgrade(self, *packages):
-        return PipMain(['install', '--upgrade', *packages])
+        return PipMain(['install', '--upgrade', *packages, '-i https://pypi.doubanio.com/simple'])
 
     def __call__(self, *args):
         return PipMain(args)
