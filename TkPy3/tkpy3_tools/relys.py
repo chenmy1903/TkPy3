51190fa1a2b6229fdf9355f831c4a5b6dac9de69
chenmy10 <chenmy10@outlook.com>
2020-06-14 16:27:34 +0800

    Signed-off-by: chenmy10 <chenmy10@outlook.com>

--- TkPy3/tkpy3_tools/relys.py
+++ TkPy3/tkpy3_tools/relys.py
@@ -11,9 +11,11 @@ from TkPy3.tkpy3_tools.pip_tools import tkpy_pip
 from TkPy3.locale_dirs import images_icon_dir, static_dir
 
 
-
 class InstallThread(QThread):
     done = pyqtSignal()
+    ok = pyqtSignal(str)
+    package_name = pyqtSignal(str)
+    falsed = pyqtSignal(str)
 
     def __init__(self, parent=None, *packages):
         QThread.__init__(self, parent)
@@ -22,7 +24,11 @@ class InstallThread(QThread):
     def run(self):
         pip = tkpy_pip()
         for i in self.packages:
-            pip.upgrade(i)
+            self.package_name.emit(i)
+            if pip.upgrade(i):
+                self.falsed.emit(i)
+            else:
+                self.ok.emit(i)
         self.done.emit()
 
 
@@ -31,7 +37,8 @@ class RelyDialog(QDialog):
         QDialog.__init__(self)
         self.resize(800, 400)
         self.setWindowTitle(f'TkPy{tkpy_version} 依赖')
-        self.setWindowIcon(QIcon(os.path.join(images_icon_dir, 'help_icons', 'tools.png')))
+        self.setWindowIcon(
+            QIcon(os.path.join(images_icon_dir, 'help_icons', 'tools.png')))
         self.layout = QVBoxLayout(self)
         self.text_show_label = QLabel()
         self.view_list = QListWidget()
@@ -43,7 +50,8 @@ class RelyDialog(QDialog):
             count = 0
             for package_name in f.read().split('\n'):
                 if package_name:
-                    self.view_list.insertItem(count, QListWidgetItem(package_name))
+                    self.view_list.insertItem(
+                        count, QListWidgetItem(package_name))
                     count += 1
         self.layout.addWidget(self.text_show_label)
         self.layout.addWidget(self.view_list)
@@ -91,20 +99,23 @@ class InstallDialog(RelyDialog):
             def done():
                 show_label.setText('<p>安装完成</p>')
                 exit_button.setDisabled(False)
+                message.append('安装完成。')
             dialog = QDialog()
             dialog.resize(600, 200)
-            dialog.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+            dialog.setWindowFlags(Qt.WindowStaysOnTopHint |
+                                  Qt.FramelessWindowHint)
             dialog.setWindowTitle('TkPy3安装')
             layout = QVBoxLayout()
             title_label = QLabel()
             show_label = QLabel()
+            message = QTextBrowser()
             exit_button = QPushButton()
             title_label.setText('<h1>TkPy3安装</h1>')
-            show_label.setText('<p>正在安装中... ...</p>')
             exit_button.setText('退出')
             exit_button.setDisabled(True)
             layout.addWidget(title_label)
             layout.addWidget(show_label)
+            layout.addWidget(message)
             layout.addWidget(exit_button)
             dialog.setLayout(layout)
             install_packages = []
@@ -113,7 +124,14 @@ class InstallDialog(RelyDialog):
 
             self.close()
             self.install_process.start()
+            message.append('开始安装。')
             exit_button.clicked.connect(dialog.close)
+            self.install_process.falsed.connect(
+                lambda p: message.append(f'Package {p} 安装失败。'))
+            self.install_process.ok.connect(
+                lambda p: message.append(f'Package {p} 安装成功。'))
+            self.install_process.package_name.connect(lambda text: (show_label.setText(
+                f'<h3>正在安装中... ...</h3><p>正在安装: {text}</p>'), message.append(f'安装 {text}')))
             self.install_process.done.connect(done)
             dialog.exec_()
 
@@ -127,5 +145,4 @@ class InstallDialog(RelyDialog):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     dialog = InstallDialog()
-    dialog.exec_()
-    sys.exit(app.exec_())
+    sys.exit(dialog.exec_())
