6571d4457ff9f92d27980439315afe617e129ba3
chenmy10 <chenmy10@outlook.com>
2020-06-18 20:16:02 +0800

    Signed-off-by: chenmy10 <chenmy10@outlook.com>

--- TkPy3/tkpy3_tools/pip_tools.py
+++ TkPy3/tkpy3_tools/pip_tools.py
@@ -8,16 +8,16 @@ class tkpy_pip(object):
         object.__init__(self)
 
     def install(self, *packages):
-        return os.system(f'{sys.executable} -m pip install {" ".join(packages)}')
+        return os.system(f'{sys.executable} -m pip install {" ".join(packages)} --timeout 1000')
 
     def uninstall(self, *packages):
         return os.system(f'{sys.executable} -m pip uninstall {" ".join(packages)} -y')
 
     def force_reinstall(self, *packages):
-        return os.system(f'{sys.executable} -m pip install {" ".join(packages)} --force-reinstall')
+        return os.system(f'{sys.executable} -m pip install {" ".join(packages)} --force-reinstall --timeout 1000')
 
     def upgrade(self, *packages):
-        return os.system(f'{sys.executable} -m pip install {" ".join(packages)} --upgrade')
+        return os.system(f'{sys.executable} -m pip install {" ".join(packages)} --upgrade --timeout 1000')
 
     def __call__(self, *args):
         return os.system(f'{sys.executable} -m pip {" ".join(args)}')
